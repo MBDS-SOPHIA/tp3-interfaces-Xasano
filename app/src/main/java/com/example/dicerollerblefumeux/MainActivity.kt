258699ddaf4d12dfacbadf55fd2b6b4c941330d6
Lefumeux Bastien <bastien.lefumeux@etu.unice.fr>
2025-01-06 11:50:59 +0100

    etape 3

--- app/src/main/java/com/example/dicerollerblefumeux/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerblefumeux/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dicerollerblefumeux
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -8,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val input_target: TextInputEditText = findViewById(R.id.target_number)
         val rollButton: Button = findViewById(R.id.button)
+        input_target.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                rollButton.isEnabled = !s.isNullOrEmpty()
+        }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
         rollButton.setOnClickListener { rollDice() }
     }
 
@@ -27,10 +39,12 @@ class MainActivity : AppCompatActivity() {
         val dice1 = Dice(6)
         val dice2 = Dice(6)
         val resultd1: TextView = findViewById(R.id.d1)
-        resultd1.text = dice1.roll().toString()
+        val sumd1 = dice1.roll().toString()
+        resultd1.text = sumd1
         val resultd2: TextView = findViewById(R.id.d2)
-        resultd2.text = dice2.roll().toString()
-        if (resultd1.text == resultd2.text) {
+        val sumd2 = dice2.roll().toString()
+        resultd2.text = sumd2
+        if (sumd1.toInt() + sumd2.toInt() == findViewById<TextInputEditText>(R.id.target_number).text.toString().toInt()) {
             Toast.makeText(applicationContext, "Victoire !", Toast.LENGTH_SHORT).show()
         }
     }
